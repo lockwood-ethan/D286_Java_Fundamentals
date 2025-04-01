7f83c876fc2fea5eca6476f0c03beb6aaec686b2
Ethan <lockwood.ethan@yahoo.com>
2025-04-01 13:18:46 -0400

    Zylabs 1.15 finish

--- Zylab1_15.java
+++ Zylab1_15.java
@@ -1,5 +1,7 @@
 public class Zylab1_15 {
     public static void main(String[] args) {
         /* Type your code here. */
+        System.out.println("  NO PARKING");
+        System.out.println("2:00 - 6:00 a.m.");
     }
 }
