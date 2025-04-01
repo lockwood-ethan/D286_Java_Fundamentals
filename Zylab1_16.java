081820e294d44af98486ed955fc34b5e41f686c4
Ethan <lockwood.ethan@yahoo.com>
2025-04-01 13:22:05 -0400

    Zylabs 1.16 start

---
+++ Zylab1_16.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Zylab1_16 {
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        int currentPrice;
+        int lastMonthsPrice;
+
+        currentPrice = scnr.nextInt();
+        lastMonthsPrice = scnr.nextInt();
+
+        /* Type your code here. */
+    }
+}
