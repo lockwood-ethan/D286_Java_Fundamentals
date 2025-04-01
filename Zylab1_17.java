12b2829ece7c3903d16be3ab8c045ef810a2d480
Ethan <lockwood.ethan@yahoo.com>
2025-04-01 13:35:04 -0400

    Zylabs 1.17 start

---
+++ Zylab1_17.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Zylab1_17 {
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        int baseChar;
+        int headChar;
+
+        /* Type your code here */
+    }
+}
