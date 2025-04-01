c3a44806a05f90a835434c351fc28587ef39b7ea
Ethan <lockwood.ethan@yahoo.com>
2025-04-01 12:25:35 -0400

    Zylabs 1.13 start

---
+++ Zylab1_13.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Zylab1_13 {
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        int x;
+
+        System.out.println("Enter x: ");
+        x = scnr.nextInt();
+
+        System.out.println(x); //Student mistakenly is echoing the input to output to match example
+        System.out.println("x doubled is: " + (2 * x));
+    }
+}
