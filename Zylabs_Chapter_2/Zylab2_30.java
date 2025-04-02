378d0f441e7ea8113c8ac5b523ed564bbfc4a220
Ethan <lockwood.ethan@yahoo.com>
2025-04-01 23:56:12 -0400

    Zylab 2.30 finish

--- Zylabs_Chapter_2/Zylab2_30.java
+++ Zylabs_Chapter_2/Zylab2_30.java
@@ -3,8 +3,19 @@ import java.util.Scanner;
 
 public class Zylab2_30 {
     public static void main(String[] args) {
-        Scanner scrn = new Scanner(System.in);
+        Scanner scnr = new Scanner(System.in);
 
         /* Type your code here. */
+        final int QUARTER = 25;
+        final int DIME = 10;
+        final int NICKEL = 5;
+        double numQuarters = scnr.nextInt();
+        double numDimes = scnr.nextInt();
+        double numNickels = scnr.nextInt();
+        double numPennies = scnr.nextInt();
+
+        double totalMoney = (numQuarters * QUARTER) + (numDimes * DIME) + (numNickels * NICKEL) + numPennies;
+        double dollars = totalMoney / 100;
+        System.out.printf("Amount: $%.2f\n", dollars);
     }
 }
