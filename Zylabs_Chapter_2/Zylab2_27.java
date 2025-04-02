f1b518f15ad8e525dbd7154966e5e7bf2a4fdb06
Ethan <lockwood.ethan@yahoo.com>
2025-04-01 22:37:19 -0400

    Zylab 2.27 start

---
+++ Zylabs_Chapter_2/Zylab2_27.java
@@ -0,0 +1,12 @@
+package Zylabs_Chapter_2;
+import java.util.Scanner;
+
+public class Zylab2_27 {
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        double x;
+        double y;
+        double z;
+        /* Type your code here */
+    }
+}
