7c864b9b1359a072be739f88d3c19bb0223f86f7
Ethan <lockwood.ethan@yahoo.com>
2025-04-01 22:57:09 -0400

    Zylab 2.28 part 1

--- Zylabs_Chapter_2/Zylab2_28.java
+++ Zylabs_Chapter_2/Zylab2_28.java
@@ -8,8 +8,17 @@ public class Zylab2_28 {
         int num2;
         int num3;
         int num4;
+        int productNums;
+        int avgNums;
 
         /* Type your code here. */
+        num1 = scnr.nextInt();
+        num2 = scnr.nextInt();
+        num3 = scnr.nextInt();
+        num4 = scnr.nextInt();
+        productNums = num1 * num2 * num3 * num4;
+        avgNums = (num1 + num2 + num3 + num4) / 4;
+        System.out.println("" + productNums + " " + avgNums);
     }
     
 }
