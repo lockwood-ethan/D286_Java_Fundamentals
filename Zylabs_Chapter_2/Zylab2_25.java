c0fc7f1e08ba294f4a4cc790c64db19ac1796cc6
Ethan <lockwood.ethan@yahoo.com>
2025-04-01 22:21:17 -0400

    Zylab 2.25 finish

--- Zylabs_Chapter_2/Zylab2_25.java
+++ Zylabs_Chapter_2/Zylab2_25.java
@@ -4,5 +4,18 @@ import java.util.Scanner;
 public class Zylab2_25 {
     public static void main(String[] args) {
         /* Type your code here. */
+        Scanner scnr = new Scanner(System.in);
+        int userNum;
+        int divNum;
+
+        userNum = scnr.nextInt();
+        divNum = scnr.nextInt();
+
+        userNum = userNum / divNum;
+        System.out.print("" + userNum + " ");
+        userNum = userNum / divNum;
+        System.out.print("" + userNum + " ");
+        userNum = userNum / divNum;
+        System.out.println("" + userNum);
     }
 }
