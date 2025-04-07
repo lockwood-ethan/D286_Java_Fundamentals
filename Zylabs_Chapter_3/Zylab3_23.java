b61d72134f68ca12e034ee31c099f015b4e8bee8
Ethan <lockwood.ethan@yahoo.com>
2025-04-07 15:16:54 -0400

    Zylabs 3.23 start

---
+++ Zylabs_Chapter_3/Zylab3_23.java
@@ -0,0 +1,15 @@
+package Zylabs_Chapter_3;
+import java.util.Scanner;
+
+public class Zylab3_23 {
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        int highwayNumber;
+        int primaryNumber;
+
+        highwayNumber = scnr.nextInt();
+
+        /* Type your code here. */
+    }
+    
+}
