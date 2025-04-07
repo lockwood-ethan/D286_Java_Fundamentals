04974813c9fd6c400f6b913dba2c2577dffdc198
Ethan <lockwood.ethan@yahoo.com>
2025-04-07 15:57:32 -0400

    Zylabs 3.23 finish

--- Zylabs_Chapter_3/Zylab3_23.java
+++ Zylabs_Chapter_3/Zylab3_23.java
@@ -8,8 +8,26 @@ public class Zylab3_23 {
         int primaryNumber;
 
         highwayNumber = scnr.nextInt();
+        primaryNumber = highwayNumber % 100;
 
         /* Type your code here. */
+        if ((highwayNumber > 0) && (highwayNumber % 100 != 0)) {
+            if ((highwayNumber > 0) && (highwayNumber < 100) && (highwayNumber % 2 == 0)) {
+                System.out.println("I-" + highwayNumber + " is primary, going east/west.");
+            }
+            else if ((highwayNumber > 0) && (highwayNumber < 100) && (highwayNumber % 2 != 0)) {
+                System.out.println("I-" + highwayNumber + " is primary, going north/south.");
+            }
+            if ((highwayNumber > 99) && (highwayNumber < 1000) && (highwayNumber % 2 == 0)) {
+                System.out.println("I-" + highwayNumber + " is auxiliary, serving I-" + primaryNumber + ", going east/west.");
+            }
+            else if ((highwayNumber > 99) && (highwayNumber < 1000) && (highwayNumber % 2 != 0)) {
+                System.out.println("I-" + highwayNumber + " is auxiliary, serving I-" + primaryNumber + ", going north/south.");
+            }
+        }
+        else {
+            System.out.println(highwayNumber +" is not a valid interstate highway number.");
+        }
     }
     
 }
