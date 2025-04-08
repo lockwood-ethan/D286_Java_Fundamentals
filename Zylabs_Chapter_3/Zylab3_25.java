e7f02b8d61082fbb5f9728b05d94a6105ecd0cd4
Ethan <lockwood.ethan@yahoo.com>
2025-04-08 09:19:22 -0400

    Zylabs 3.25 start

---
+++ Zylabs_Chapter_3/Zylab3_25.java
@@ -0,0 +1,16 @@
+package Zylabs_Chapter_3;
+import java.util.Scanner;
+
+public class Zylab3_25 {
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        int inputYear;
+        boolean isLeapYear;
+
+        isLeapYear = false;
+        inputYear = scnr.nextInt();
+
+        /* Type your code here. */
+    }
+    
+}
