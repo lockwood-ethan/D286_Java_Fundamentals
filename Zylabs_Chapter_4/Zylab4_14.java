6411ca7f2bb89be44f561acf5a660070c911ae65
Ethan <lockwood.ethan@yahoo.com>
2025-04-09 16:24:05 -0400

    Zylabs 4.14 finish

--- Zylabs_Chapter_4/Zylab4_14.java
+++ Zylabs_Chapter_4/Zylab4_14.java
@@ -4,6 +4,15 @@ import java.util.Scanner;
 public class Zylab4_14 {
     public static void main(String[] args) {
         /* Type your code here. */
+        Scanner scnr = new Scanner(System.in);
+        int x;
+
+        x = scnr.nextInt();
+
+        while (x > 0) {
+            System.out.print(x % 2);
+            x = x / 2;
+        }
+        System.out.println();
     }
-    
 }
