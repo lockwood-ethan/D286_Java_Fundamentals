e22583a337620663300a0590aeab8445848c7cc9
Ethan <lockwood.ethan@yahoo.com>
2025-04-11 14:28:41 -0400

    Zylabs 4.19 finish

--- Zylabs_Chapter_4/Zylab4_19.java
+++ Zylabs_Chapter_4/Zylab4_19.java
@@ -4,5 +4,22 @@ import java.util.Scanner;
 public class Zylab4_19 {
     public static void main(String[] args) {
         /* Type your code here. */
+        Scanner scnr = new Scanner(System.in);
+        String userInput;
+
+        userInput = "";
+
+        while (scnr.hasNext()) {
+            if (scnr.hasNextLine()) {
+                userInput = scnr.nextLine();
+                if (userInput.equals("Done") || userInput.equals("done") || userInput.equals("d")) {
+                    break;
+                }
+                for (int i = userInput.length() - 1; i >= 0; --i) {
+                    System.out.print("" + userInput.charAt(i));
+                }
+            }
+            System.out.println("");
+        }
     }
 }
