ead4c670ab0d0dc7c179eebefb54db6d6a0ffc86
Ethan <lockwood.ethan@yahoo.com>
2025-04-11 13:34:10 -0400

    Zylabs 4.17 finish

--- Zylabs_Chapter_4/Zylab4_17.java
+++ Zylabs_Chapter_4/Zylab4_17.java
@@ -4,5 +4,18 @@ import java.util.Scanner;
 public class Zylab4_17 {
     public static void main(String[] args) {
         /* Type your code here. */
+        Scanner scnr = new Scanner(System.in);
+        String userInput;
+        String newString;
+
+        userInput = scnr.nextLine();
+        newString = "";
+
+        for (int i = 0; i < userInput.length(); ++i) {
+            if (Character.isLetter(userInput.charAt(i))) {
+                newString = newString + userInput.charAt(i);
+            }
+        }
+        System.out.println(newString);
     }
 }
