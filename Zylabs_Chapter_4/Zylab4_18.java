08ba206633fadb5197339d05a11718149bf50dba
Ethan <lockwood.ethan@yahoo.com>
2025-04-11 13:54:46 -0400

    Zylabs 4.18 finish

--- Zylabs_Chapter_4/Zylab4_18.java
+++ Zylabs_Chapter_4/Zylab4_18.java
@@ -4,5 +4,22 @@ import java.util.Scanner;
 public class Zylab4_18 {
     public static void main(String[] args) {
         /* Type your code here. */
+        Scanner scnr = new Scanner(System.in);
+        int firstInt;
+        int secondInt;
+
+        firstInt = scnr.nextInt();
+        secondInt = scnr.nextInt();
+
+        if (secondInt < firstInt) {
+            System.out.println("Second integer can't be less than the first.");
+        }
+        else {
+            while (firstInt <= secondInt) {
+                System.out.print("" + firstInt + " ");
+                firstInt = firstInt + 5;
+            }
+            System.out.println("");
+        }
     }
 }
