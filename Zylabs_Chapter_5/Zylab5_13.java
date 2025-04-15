0cbcc7ae6cc52ad0581752ea81291414262621cf
Ethan <lockwood.ethan@yahoo.com>
2025-04-15 15:41:55 -0400

    Zylabs 5.13 start

---
+++ Zylabs_Chapter_5/Zylab5_13.java
@@ -0,0 +1,15 @@
+package Zylabs_Chapter_5;
+import java.util.Scanner;
+
+public class Zylab5_13 {
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        int[] userList = new int[20];
+        int numElements;
+
+
+        numElements = scnr.nextInt();
+
+        /* Type your code here. */
+    }
+}
--- Zylabs_Chapter_5/notes.java
+++
@@ -1,9 +0,0 @@
-package Zylabs_Chapter_5;
-
-import java.util.Dictionary;
-
-public class notes {
-    public static void main(String[] args) {
-        
-    }
-}
