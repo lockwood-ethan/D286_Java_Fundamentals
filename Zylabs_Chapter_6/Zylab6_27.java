f764b3fc9734920df3a66f534a93b8f48ef19d8a
Ethan <lockwood.ethan@yahoo.com>
2025-04-17 16:50:42 -0400

    Zylabs 6.27 start

---
+++ Zylabs_Chapter_6/Zylab6_27.java
@@ -0,0 +1,15 @@
+package Zylabs_Chapter_6;
+import java.util.Scanner;
+import java.util.Random;
+
+public class Zylab6_27 {
+
+    // TODO: Define your method here
+
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        Random rand = new Random(2);
+
+        // TODO: Complete main here
+    }
+}
