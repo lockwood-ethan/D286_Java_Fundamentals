d5b2fd541910d2f7106f0ab1535ce4fb8e7bd432
Ethan <lockwood.ethan@yahoo.com>
2025-04-17 15:43:59 -0400

    Zylabs 6.26 start

---
+++ Zylabs_Chapter_6/Zylab6_26.java
@@ -0,0 +1,10 @@
+package Zylabs_Chapter_6;
+import java.util.Scanner;
+
+public class Zylab6_26 {
+    /* Define your method here */
+
+    public static void main(String[] args) {
+        /* Type your code here. */
+    }
+}
