736c819e637d9639e6d3e46c081b6a93f36eab42
Ethan <lockwood.ethan@yahoo.com>
2025-04-17 11:36:10 -0400

    Zylabs 5.18 finish

--- Zylabs_Chapter_5/Zylab5_18.java
+++ Zylabs_Chapter_5/Zylab5_18.java
@@ -4,5 +4,22 @@ import java.util.Scanner;
 public class Zylab5_18 {
     public static void main(String[] args) {
         /* Type your code here. */
+        Scanner scnr = new Scanner(System.in);
+        int numValues = scnr.nextInt();
+        String[] stringValues = new String[20];
+
+        for (int i = 0; i < numValues; ++i) {
+            stringValues[i] = scnr.next();
+        }
+
+        String magicLetter = scnr.next();
+
+        for (int i = 0; i < numValues; ++i) {
+            if (stringValues[i].contains(magicLetter)) {
+                System.out.print("" + stringValues[i] + ",");
+            }
+        }
+        System.out.println();
+
     }
 }
