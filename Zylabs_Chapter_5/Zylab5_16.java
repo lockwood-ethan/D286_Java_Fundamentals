a2018ac9aa7894c9e2646be67c372a5b44e03943
Ethan <lockwood.ethan@yahoo.com>
2025-04-17 10:22:37 -0400

    Zylabs 5.16 finish

--- Zylabs_Chapter_5/Zylab5_16.java
+++ Zylabs_Chapter_5/Zylab5_16.java
@@ -4,5 +4,23 @@ import java.util.Scanner;
 public class Zylab5_16 {
     public static void main(String[] args) {
         /* Type your code here. */
+        Scanner scnr = new Scanner(System.in);
+        double[] userValues = new double[20];
+        
+        int numValues = scnr.nextInt();
+        double maxValue = userValues[0];
+
+        for (int i = 0; i < numValues; ++i) {
+            userValues[i] = scnr.nextDouble();
+            if (userValues[i] > maxValue) {
+                maxValue = userValues[i];
+            }
+        }
+        for (int i = 0; i < numValues; ++i) {
+            userValues[i] = userValues[i] / maxValue;
+            System.out.printf("%.2f", userValues[i]);
+            System.out.print(" ");
+        }
+        System.out.println();
     }
 }
