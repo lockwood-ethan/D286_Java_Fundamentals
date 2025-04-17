cca467335f1c689e9c6902a7b2d67336acf1fef0
Ethan <lockwood.ethan@yahoo.com>
2025-04-17 17:28:50 -0400

    Zylabs 6.27 finish

--- Zylabs_Chapter_6/Zylab6_27.java
+++ Zylabs_Chapter_6/Zylab6_27.java
@@ -5,11 +5,28 @@ import java.util.Random;
 public class Zylab6_27 {
 
     // TODO: Define your method here
+    public static String coinFlip(Random rand) {
+        int resultNumber = rand.nextInt(2);
+        String coinString;
+        if (resultNumber == 0) {
+            coinString = "Tails";
+        }
+        else {
+            coinString = "Heads";
+        }
+        return coinString;
+    }
 
     public static void main(String[] args) {
         Scanner scnr = new Scanner(System.in);
         Random rand = new Random(2);
+        int userInput = scnr.nextInt();
 
         // TODO: Complete main here
+        for (int i = 0; i < userInput; ++i) {
+            String result = coinFlip(rand);
+            System.out.println(result);
+        }
+
     }
 }
