7932fc56fafbac2bfbda8c1a12cf4656cb2ae9bc
Ethan <lockwood.ethan@yahoo.com>
2025-04-16 23:36:23 -0400

    Zylabs 5.14 start

---
+++ Zylabs_Chapter_5/Zylab5_14.java
@@ -0,0 +1,11 @@
+package Zylabs_Chapter_5;
+import java.util.Scanner;
+
+public class Zylab5_14 {
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        int[] userValues = new int[9];
+
+        /* Type your code here. */
+    }
+}
