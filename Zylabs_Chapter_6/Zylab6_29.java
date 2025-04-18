07ae63391603ded531004d6bc87781956eb5ad5c
Ethan Lockwood <lockwood.ethan@yahoo.com>
2025-04-18 13:52:10 -0400

    Zylabs 6.29 finish

--- Zylabs_Chapter_6/Zylab6_29.java
+++ Zylabs_Chapter_6/Zylab6_29.java
@@ -4,8 +4,29 @@ import java.util.Scanner;
 public class Zylab6_29 {
 
     /* Define your method here */
+    public static int getWordFrequency(String[] wordsList, int listSize, String currWord) {
+        int count = 0;
+        for (int i = 0; i < listSize; ++i) {
+            if (currWord.equalsIgnoreCase(wordsList[i])) {
+                count += 1;
+            }
+        }
+        return (count);
+    }
 
     public static void main(String[] args) {
         /* Type your code here. */
+        Scanner scnr = new Scanner(System.in);
+        String[] wordsList = new String[20];
+        int wordCount;
+        int listSize = scnr.nextInt();
+
+        for (int i = 0; i < listSize; ++i) {
+            wordsList[i] = scnr.next();
+        }
+        for (int i = 0; i < listSize; ++i) {
+            wordCount = getWordFrequency(wordsList, listSize, wordsList[i]);
+            System.out.println("" + wordsList[i] + " " + wordCount);
+        }
     }
 }
